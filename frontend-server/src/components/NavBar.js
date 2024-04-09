93f3a81e194e6498e85cbc3736833c2c64f70fb5
arpit5492 <arpitmallick99@gmail.com>
2024-04-09 10:48:50 -0700

    Minor changes in css

--- frontend-server/src/components/NavBar.js
+++ frontend-server/src/components/NavBar.js
@@ -1,23 +1,28 @@
 const NavBar = () => {
+    const selData = [
+        "Men", "Women", "Kids"
+    ]
     return (
         <div className='header bg-dark'>
-        <div className='row'>
-          <div className='brand my-1'>
-            <h1> Market Verse </h1>
-          </div>
-          <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
-            <div className='dropdown m-0 p-0'>
-              <select className='select-btn w-100 p-0 m-0'>
-                <option> Men </option>
-                <option> Women </option>
-                <option> Kids </option>
-              </select>
+            <div className='row'>
+                <div className='brand'>
+                    <h3 className="">Market Verse</h3>
+                </div>
+                <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
+                    <div className='dropdown m-0 p-0'>
+                        <select className='select-btn w-100 p-0 m-0'>
+                            {selData.map((data, index) => {
+                                return (
+                                    <option key={index}>{data}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <input className='form-control ' placeholder='Search...'/>
+                    <button> <i className='bi bi-search'/> </button>
+                </div>
             </div>
-            <input className='form-control ' placeholder='Search...'/>
-            <button> <i className='bi bi-search'/> </button>
-          </div>
         </div>
-      </div>
     )
 }
 
