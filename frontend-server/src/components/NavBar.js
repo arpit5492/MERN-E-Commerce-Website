22ede55d5d5e13978933e35de1ebdd3db23c3887
arpit5492 <arpitmallick99@gmail.com>
2024-04-09 01:09:22 -0700

    Navbar implemented

---
+++ frontend-server/src/components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <a className="navbar-brand text-light mx-4 py-2 navFont" href="">Market Verse</a>
+        </nav>
+    )
+}
+
+export default NavBar;
