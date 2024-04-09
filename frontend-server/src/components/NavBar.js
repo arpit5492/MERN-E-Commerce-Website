348f382406e4ef952f5d658ca0e3b8c22b14dfdd
arpit5492 <arpitmallick99@gmail.com>
2024-04-09 02:41:04 -0700

    Added search bar

--- frontend-server/src/components/NavBar.js
+++ frontend-server/src/components/NavBar.js
@@ -1,8 +1,16 @@
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-            <a className="navbar-brand text-light mx-4 py-2 navFont" href="">Market Verse</a>
-        </nav>
+        <div className="navbar navbar-light bg-dark row">
+            <div className="navFont col-lg-6">
+                <h1 className="navbar-brand text-light mx-4 py-2">Market Verse</h1>
+            </div>
+            <div className="p-0 my-2 h-25 col-lg-6">
+                <form className="search-form" action="">
+                    <input type="search" className="form-control" placeholder="Search..."/>
+                    <button className="bi bi-search"></button>
+                </form>
+            </div>
+        </div>
     )
 }
 
