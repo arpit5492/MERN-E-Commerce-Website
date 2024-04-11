b6a6070b4bfa9cb69f2c008b90908d32edcc05c4
arpit5492 <arpitmallick99@gmail.com>
2024-04-11 00:50:08 -0700

    Added multiple prods and mapped them

--- frontend-server/src/components/ProdComp/index.js
+++ frontend-server/src/components/ProdComp/index.js
@@ -1,17 +1,56 @@
 import "./_prod-comp.scss";
+import {useState} from "react";
 
 const ProdComp = () => {
+  const prodData = [
+    {
+      pName: "Jacket",
+      price: 45,
+      img: "https://m.media-amazon.com/images/I/81iOSL3bIfL._AC_SX679_.jpg"
+    },
+    {
+      pName: "Shirt",
+      price: 29,
+      img: "https://m.media-amazon.com/images/I/61JELqH2RvL._AC_SX679_.jpg"
+    },
+    {
+      pName: "Jeans",
+      price: 38,
+      img: "https://i.insider.com/64493a3790523900196ff87d?width=1005&format=jpeg&auto=webp"
+    },
+    {
+      pName: "Boots",
+      price: 70,
+      img: "https://m.media-amazon.com/images/I/717LvSTNqQL._AC_SY695_.jpg"
+    },
+    {
+      pName: "Top",
+      price: 43.99,
+      img: "https://m.media-amazon.com/images/I/51CFDAxtC3L._AC_SY879_.jpg"
+    },
+    {
+      pName: "Top",
+      price: 43.99,
+      img: "https://m.media-amazon.com/images/I/51CFDAxtC3L._AC_SY879_.jpg"
+    }
+  ]
   return (
     <div className="prod-comp">
-      <div className="mx-5 p-3 col-lg-3 col-md-6 prod-card">
-        <div className="prod-img">
-          <img src="https://m.media-amazon.com/images/I/81iOSL3bIfL._AC_SX679_.jpg" alt="" />
-        </div>
-        <div className="prod-text">
-          <h5><a href="#">Jacket</a></h5>
-          <p> $45 </p>
-        </div>
-      </div>
+      {
+        prodData.map(item => {
+          return (
+            <div className="mx-4 my-3 p-3 prod-card">
+              <div className="prod-img">
+                <img src={item.img} alt={item.pName} />
+              </div>
+              <div className="prod-text">
+                <h5><a href="#">{item.pName}</a></h5>
+                <p> ${item.price} </p>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
