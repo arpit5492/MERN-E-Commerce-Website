a48c1152fc30067a445d0047c44c7392b3044415
arpit5492 <arpitmallick99@gmail.com>
2024-04-10 22:21:25 -0700

    Fixed some styling on the prod text

--- frontend-server/src/components/ProdComp/index.js
+++ frontend-server/src/components/ProdComp/index.js
@@ -3,11 +3,11 @@ import "./_prod-comp.scss";
 const ProdComp = () => {
   return (
     <div className="prod-comp">
-      <div className="mx-5 p-3 col-lg-3 col-md-6 prod">
+      <div className="mx-5 p-3 col-lg-3 col-md-6 prod-card">
         <div className="prod-img">
           <img src="https://m.media-amazon.com/images/I/81iOSL3bIfL._AC_SX679_.jpg" alt="" />
         </div>
-        <div>
+        <div className="prod-text">
           <h5><a href="#">Jacket</a></h5>
           <p> $45 </p>
         </div>
