442a3f9b4ae7311d3909a7e25472df8465b7b300
arpit5492 <arpitmallick99@gmail.com>
2024-04-12 10:09:00 -0700

    Removed the getConnection() method

--- backend-server/index.js
+++ backend-server/index.js
@@ -12,22 +12,16 @@ app.use(morgan("dev"));
 //Routes
 app.get("/", (req, res) => {
   let tableData;
-  pool.getConnection((err, connection) => {
-    if(err) {
-      res.status(500).send(err)
-    } else {
-      pool.query("select * from categories", (err, data) => {
-        if(err) {
-          tableData = err;
-          res.status(500).send(tableData);
-        } else {
-          tableData = data;
-          res.status(200).send(tableData);
-        }
-      })
-    }
-  })
-});
+    pool.query("select * from categories", (err, data) => {
+      if(err) {
+        tableData = err;
+        res.status(500).send(tableData);
+      } else {
+        tableData = data;
+        res.status(200).send(tableData);
+      }
+    });
+  });
 
 app.listen(port, () => {
   console.log(`The server is running on: http://localhost:${port}`);
