b0c04d1cfae119ca426eb65442ff0809c8ee0181
arpit5492 <arpitmallick99@gmail.com>
2024-04-12 10:17:34 -0700

    Created different files for routes

--- backend-server/index.js
+++ backend-server/index.js
@@ -1,27 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import prodCat from "./routes/prodCat.js";
 import dotenv from "dotenv";
-import pool from "./config/index.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 //Middleware functions
 app.use(morgan("dev"));
-
-//Routes
-app.get("/", (req, res) => {
-  let tableData;
-    pool.query("select * from categories", (err, data) => {
-      if(err) {
-        tableData = err;
-        res.status(500).send(tableData);
-      } else {
-        tableData = data;
-        res.status(200).send(tableData);
-      }
-    });
-  });
+app.use("/prodCat", prodCat);
 
 app.listen(port, () => {
   console.log(`The server is running on: http://localhost:${port}`);
