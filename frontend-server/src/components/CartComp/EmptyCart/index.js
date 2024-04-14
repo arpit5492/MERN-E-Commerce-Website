6719e4627a609432d24903e4426287a033232a60
arpit5492 <arpitmallick99@gmail.com>
2024-04-14 12:06:05 -0700

    Styled the empty cart comp

--- frontend-server/src/components/CartComp/EmptyCart/index.js
+++ frontend-server/src/components/CartComp/EmptyCart/index.js
@@ -1,9 +1,17 @@
 import "./_empty-cart.scss";
+import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
   return (
-    <div>
-      <h2>The Cart is Empty</h2>
+    <div className="p-4 ec-main-div">
+      <span className="my-5 ec-text">The Cart is Empty</span>
+      <hr />
+      <Link to="/">
+        <div className="btn btn-warning my-3">
+          <span>Continue shopping</span>
+        </div>
+      </Link>
+      
     </div>
   )
 }
