e5ba05f56307536b003776be12f75ef289ba7b74
arpit5492 <arpitmallick99@gmail.com>
2024-04-14 00:48:18 -0700

    Added home route using react router

--- frontend-server/src/App.js
+++ frontend-server/src/App.js
@@ -2,13 +2,16 @@ import './App.scss';
 import CatNav from './components/CatNav';
 import NavBar from "./components/NavBar";
 import MainComp from './components/MainComp';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <CatNav />
-      <MainComp />
+      <Routes>
+        <Route path='/' element={<MainComp/>}/>
+      </Routes>
     </div>
   );
 }
